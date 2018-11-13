981a2f68394fce923540bca5bd949e6ef932be49
spencertollefson <spencertollefson@gmail.com>
2018-11-12 18:57:18 -0800

    make some good headway with cleaning data better and looking to plot results

--- utils.py
+++ utils.py
@@ -13,12 +13,18 @@ def string_clean_df_column(df, col_name):
     '''
    
     # remove URLs and parantheses surrounding URLs
-    df[col_name] = df[col_name].str.replace(r"(\()*https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(\))*",' ', regex=True)
+    df[col_name] = df[col_name].str.replace(r"(\()*https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(\))*"," ", regex=True)
+    
+    # Chance strange character to contraction symbol
+    df[col_name] = df[col_name].str.replace(r"’", "'", regex=True)
 
     # Remove non-standard characters and numbers
     df[col_name] = df[col_name].str.replace(r"[^A-Za-z(),!?@\'\`\"\_\n]", " ", regex=True)
     df[col_name] = df[col_name].str.replace(r"@", "at", regex=True)
 
+    # Drop other non-alphas
+    df[col_name] = df[col_name].str.replace(r"[(),!?\`\"\_]", " ", regex=True)
+    
     # Strip whitespace
     df[col_name] = df[col_name].str.strip()
 
@@ -26,12 +32,9 @@ def string_clean_df_column(df, col_name):
     df[col_name] = df[col_name].str.lower()
 
     # Remove extra spaces
-    df[col_name].replace(r"\s+",' ', regex=True, inplace=True)
+    df[col_name].replace(r"\s+"," ", regex=True, inplace=True)
     
-    # Drop any 
-    df[col_name].drop(index = df[df.body == 'full quote'].index, inplace=True)
-    
-    # Drop other non-alphas
-    df[col_name] = df[col_name].str.replace(r"[(),!?@\'\`\"\_]", "", regex=True)
+    # Drop any cell with only "full quote" as the body
+    df = df.loc[df.body != 'full quote', :]
 
     return df
